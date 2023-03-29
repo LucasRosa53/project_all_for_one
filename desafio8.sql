7cdf79183c12988dc5d319a90b798924e9d36329
LucasRosa53 <lucasrosa223@hotmail.com>
2023-03-28 21:35:11 -0300

    resolvendo requisito 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,3 +1,3 @@
-SELECT CONCAT(first_name, " ",  last_name) AS "full_name" FROM employees;
-
-SELECT CONCAT(city, "-", state_province, ", ", address) AS "location" FROM employees;
+SELECT CONCAT(first_name, " ",  last_name) AS "full_name", 
+(city, "-", state_province, ", ", address) AS "location" 
+FROM employees;
